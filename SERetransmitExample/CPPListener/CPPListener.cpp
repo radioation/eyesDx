54a893f4f931b4bd305294daadbd5596a64d05a3
radioation <greggallardo@gmail.com>
2018-07-23 13:44:55 -0500

    making code work in Ubuntu 16.04.1 g++   5.4.0-6ubuntu1~16.04.10[D

--- SERetransmitExample/CPPListener/CPPListener.cpp
+++ SERetransmitExample/CPPListener/CPPListener.cpp
@@ -1,76 +1,110 @@
 // CPPListener.cpp : Defines the entry point for the console application.
 //
 
+#include <iostream>
+#include <string>
+
+#ifdef _WIN32
 #include "stdafx.h"
 #include <winsock2.h>
 #include <WS2tcpip.h>
-#include <iostream>
 #pragma comment(lib,"ws2_32.lib") // link against Winsock Library
+#else
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h> 
+#endif
 
 #define SERVER "127.0.0.1"  
-#define BUFLEN 2048  
+#define BUF_LEN 2048  
 #define PORT 51500
 
 int main()
 {
 	struct sockaddr_in address;
-	int s, slen = sizeof(address);
-	char buf[BUFLEN];
-	char message[BUFLEN];
+	int s = 0;
+	socklen_t alen = sizeof(address);
+	char buf[BUF_LEN];
+	char message[BUF_LEN];
+
+#ifdef _WIN32
 	WSADATA wsa;
+
 
 	// startup Winsock
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
-		std::cerr << "WSAStartup() failed with : " << WSAGetLastError() << std::endl;
-		exit(EXIT_FAILURE);
+		std::cerr << "WSAStartup() failed" << std::endl;
+		return -1;
 	}
+#endif
 
 	// Create the UDP socket
-	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
+	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
 	{
-		std::cerr << "socket() failed with : " << WSAGetLastError() << std::endl;
-		exit(EXIT_FAILURE);
+		std::cerr << "socket() failed" << std::endl;
+		return -2;
 	}
 
 	//setup address structure
 	memset((char *)&address, 0, sizeof(address));
 	address.sin_family = AF_INET;
 	address.sin_port = htons(PORT);
-	// address.sin_addr.S_un.S_addr = inet_addr(SERVER);
+#ifdef _WIN32 
 	InetPton(AF_INET, _T(SERVER), &address.sin_addr.s_addr);
+#else
+	inet_pton(AF_INET, SERVER, &address.sin_addr.s_addr);
+#endif
 
 
-	if (bind(s, (struct sockaddr *)&address, sizeof(address)) == SOCKET_ERROR)
+	if (bind(s, (struct sockaddr *)&address, sizeof(address)) < 0)
 	{
-		printf("Bind failed with error code : %d", WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-	puts("Bind done");
+		std::cerr << "bind() failed" << std::endl;
+		return -3;
+
+	} 
 	
-	while (1)
+	for( int i=0; i < 9000; ++i )
 	{
 		// wait for message from adapter
-		memset(buf, '\0', BUFLEN);
-		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &address, &slen) == SOCKET_ERROR)
+		memset(buf, 0, BUF_LEN);
+		if (recvfrom(s, buf, BUF_LEN, 0, (struct sockaddr *) &address, &alen) < 0)
 		{
-			std::cerr << "recvfrom() failed with : " << WSAGetLastError() << std::endl;
-			exit(EXIT_FAILURE);
+			std::cerr << "recvfrom() failed" << std::endl;
+			return -4;
 		}
+
+		// do conversions
 		float x, y;
-		INT64 timestamp = 0;
-		memcpy(&timestamp, buf, sizeof(INT64));
-		int offset = sizeof(INT64);
+		int64_t timestamp = 0;
+		memcpy(&timestamp, buf, sizeof(int64_t));
+		int offset = sizeof(int64_t);
 		memcpy(&x, buf + offset, 4);
 		offset += 4;
 		memcpy(&y, buf + offset, 4);
 		offset += 4;
-		std::cout << "Filetime: " << timestamp << " X: " << x << " y: " << y  << " objectName: " << buf + offset << std::endl;
-		// do conversions
+		std::string name = buf + offset;
+		offset += 64;
+		float lopen, ropen, eopen;
+		memcpy(&lopen, buf + offset, 4);
+		offset += 4;
+		memcpy(&ropen, buf + offset, 4);
+		offset += 4;
+		memcpy(&eopen, buf + offset, 4);
+		offset += 4;
+		std::cout << "Filetime: " << timestamp << " X: " << x << " y: " << y  << " objectName: " << name << std::endl;
+		std::cout << "left eyelid opening:  " << lopen << " Right eyelid opening: " << ropen << " eyelid opening: " << eopen << std::endl;
+		
 	}
 
+#ifdef _WIN32
 	closesocket(s);
+
 	WSACleanup();
+#endif
 
 	return 0;
 }
