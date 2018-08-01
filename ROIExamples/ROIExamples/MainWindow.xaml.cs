b37bfaff47d6bcd6d191d8aa67fcee3b37230267
radioation <greggallardo@gmail.com>
2018-07-31 23:04:30 -0500

    adding simple preset save/deletion

--- ROIExamples/ROIExamples/MainWindow.xaml.cs
+++ ROIExamples/ROIExamples/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Xml.Serialization;
 
+using System.IO;
+
 namespace ROIExamples
 {
 
@@ -33,13 +35,16 @@ namespace ROIExamples
         // setup shape list
         public ObservableCollection<string> shapeList = new ObservableCollection<string>();
 
+        public ObservableCollection<string> presetList = new ObservableCollection<string>();
+        public string presetPath = System.IO.Path.GetTempPath() + "eyesDx_roi_presets.xml";
+        Dictionary<string, string> presets = new Dictionary<string, string>();
 
         static async Task<string> DeleteROIAsync(string uri, string command)
         {
             using (var client = new HttpClient())
             {
                 // connect the client to the DELETE URL ( http://<host>:<port> )
-                string data = ""; 
+                string data = "";
                 client.BaseAddress = new Uri(uri);
                 client.DefaultRequestHeaders.Accept.Clear();
 
@@ -56,7 +61,7 @@ namespace ROIExamples
                 return data;
             }
         }
-        
+
         static async Task<string> GetROIAsync(string uri, string command)
         {
             using (var client = new HttpClient())
@@ -112,6 +117,16 @@ namespace ROIExamples
             shapeList.Add("Points");
             putShapeComboBox.ItemsSource = shapeList;
             putShapeComboBox.SelectedIndex = 0;
+
+            presetList.Add("rect_test");
+            presetList.Add("ellipse_test");
+            presetList.Add("shape_test");
+            putPresetComboBox.ItemsSource = presetList;
+
+            if (File.Exists(presetPath))
+            {
+                // read in text file 
+            }
         }
 
         private async void deleteButton_Click(object sender, RoutedEventArgs e)
@@ -168,7 +183,7 @@ namespace ROIExamples
             string uri = "http://" + hostTextBox.Text + ":" + portTextBox.Text;
 
             // build command string based on properties.
-            string command = "/roi"; 
+            string command = "/roi";
 
             if (putShapeComboBox.Text == "Ellipse" || putShapeComboBox.Text == "Rectangle")
             {
@@ -185,10 +200,11 @@ namespace ROIExamples
 
                 if (putDelayTextBox.Text.Length > 0)
                     command += "/frame";
-                if( putShapeComboBox.Text == "Rectangle")
+                if (putShapeComboBox.Text == "Rectangle")
                 {
                     command += "?shape=rectangular";
-                } else
+                }
+                else
                 {
                     command += "?shape=ellipse";
                 }
@@ -219,10 +235,11 @@ namespace ROIExamples
                 try
                 {
                     var delay = Convert.ToDouble(putDelayTextBox.Text);
-                    if( delay == 0.0)
+                    if (delay == 0.0)
                     {
                         command += "&time=0";
-                    } else
+                    }
+                    else
                     {
                         // add delay to the current filetime
                         var now = DateTime.Now.ToFileTimeUtc();
@@ -238,10 +255,11 @@ namespace ROIExamples
                 }
             }
 
-            if( putScreenTextBox.Text.Length > 0)
+            if (putScreenTextBox.Text.Length > 0)
             {
                 command += "&screen_index=" + putScreenTextBox.Text;
             }
+
             textBlock.Inlines.Add("--------------------------------------------------\n");
             textBlock.Inlines.Add(String.Format("Do PUT: {0}\n", uri + command));
 
@@ -249,5 +267,145 @@ namespace ROIExamples
             await task;
         }
 
+        private void savePresetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // if save
+            if (putNameTextBox.Text == "rect_test" || putNameTextBox.Text == "ellipse_test" || putNameTextBox.Text == "shape_test")
+            {
+                //
+                MessageBox.Show("Preset name cannot be one of\n'rect_test', 'ellipse_test', 'shape_test'", "Invalid Preset Name", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if (putNameTextBox.Text == "")
+            {
+                MessageBox.Show("Preset must have a name", "Invalid Preset Name", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var name = putNameTextBox.Text;
+            var values = name + ":" + putShapeComboBox.SelectedIndex;
+
+            if (putShapeComboBox.SelectedIndex < 2)
+            {
+                // rect or ellipse
+                values += ":" + putXTextBox.Text;
+                values += ":" + putYTextBox.Text;
+                values += ":" + putWTextBox.Text;
+                values += ":" + putHTextBox.Text;
+                values += "::" + putScreenTextBox.Text;
+            }
+            else
+            {
+                values += ":::::" + putPointsTextBox.Text + ":" + putScreenTextBox.Text;
+            }
+
+            // add to dictionary
+            presets[name] = values;
+
+            // if name insn't in presetList, add it
+            if (!presetList.Contains(name))
+            {
+                presetList.Add(name);
+            }
+        }
+
+        private void deletePresetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if( putPresetComboBox.SelectedIndex < 3)
+            {
+                MessageBox.Show("Cannot delete\n'rect_test', 'ellipse_test', 'shape_test'", "Invalid Preset", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var name = putPresetComboBox.Text;
+            if(presets.ContainsKey(name))
+            {
+                presets.Remove(name);
+                presetList.Remove(name);
+                putPresetComboBox.SelectedIndex = -1;
+
+            }
+        }
+
+
+        private void putPresetComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // defaults take priority
+            var selection = (sender as ComboBox).SelectedItem as string;
+            if( selection == null)
+            {
+                putNameTextBox.Text = "";
+                putXTextBox.Text = "";
+                putYTextBox.Text = "";
+                putWTextBox.Text = "";
+                putHTextBox.Text = "";
+                putPointsTextBox.Text = "";
+                putScreenTextBox.Text = "";
+            }
+            else if (selection == "rect_test")
+            {
+                putNameTextBox.Text = "rect_test";
+                putXTextBox.Text = "0.1";
+                putYTextBox.Text = "0.1";
+                putWTextBox.Text = "0.8";
+                putHTextBox.Text = "0.8";
+                putShapeComboBox.SelectedIndex = 0;
+                putPointsTextBox.Text = "";
+                putScreenTextBox.Text = "0";
+            }
+            else if (selection == "ellipse_test")
+            {
+                putNameTextBox.Text = "ellipse_test";
+                putXTextBox.Text = "0.5";
+                putYTextBox.Text = "0.5";
+                putWTextBox.Text = "0.2";
+                putHTextBox.Text = "0.4";
+                putShapeComboBox.SelectedIndex = 1;
+                putPointsTextBox.Text = "";
+                putScreenTextBox.Text = "0";
+            }
+            else if (selection == "shape_test")
+            {
+                putNameTextBox.Text = "shape_test";
+                putXTextBox.Text = "";
+                putYTextBox.Text = "";
+                putWTextBox.Text = "";
+                putHTextBox.Text = "";
+                putShapeComboBox.SelectedIndex = 2;
+                putPointsTextBox.Text = "0.2,0.2;0.8,0.5;0.5,0.9";
+                putScreenTextBox.Text = "0";
+
+            }
+            else
+            {
+                // see if presets has name 
+                if (presets.ContainsKey(selection))
+                { 
+
+                    string[] values = presets[selection].Split(':');
+                    putNameTextBox.Text = selection;
+                    int shapeType = Int32.Parse(values[1]);
+                    putShapeComboBox.SelectedIndex = shapeType;
+                    if (shapeType < 2)
+                    {
+                        putXTextBox.Text = values[2];
+                        putYTextBox.Text = values[3];
+                        putWTextBox.Text = values[4];
+                        putHTextBox.Text = values[5];
+                        putPointsTextBox.Text = "";
+                        putScreenTextBox.Text = values[7];
+                    }
+                    else
+                    {
+                        putXTextBox.Text = "";
+                        putYTextBox.Text = "";
+                        putWTextBox.Text = "";
+                        putHTextBox.Text = "";
+                        putPointsTextBox.Text = values[6];
+                        putScreenTextBox.Text = values[7];
+                    }
+                }
+            }
+        }
     }
 }
